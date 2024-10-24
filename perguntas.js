c70f3fdaab24051a0d76288ec2648bd30a0d71c8
tomate8051 <160138669+tomate8051@users.noreply.github.com>
2024-10-24 12:20:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,61 @@
+criaCartao(
+    '',
+    ' Qual é a fórmula da segunda lei de Newton?',
+    'F=m⋅a, onde F é a força, m é a massa, e a é a aceleração. '
+)
+criaCartao(
+    '',
+    'O que é a velocidade?',
+    'Velocidade é a taxa de variação da posição em relação ao tempo'
+)
+criaCartao(
+    '',
+    'O que é a Primeira Lei da Termodinâmica?',
+    'A Primeira Lei da Termodinâmica afirma que a variação da energia interna de um sistema é igual ao calor adicionado ao sistema menos o trabalho realizado pelo sistema: ΔU=Q - W / Delta, onde ΔU é a variação da energia interna, QQQ é o calor e WWW é o trabalho.'
+)
+criaCartao(
+    '',
+    'O que diz o princípio da conservação de energia?',
+    'A energia total de um sistema isolado permanece constante, ou seja, a energia não pode ser criada nem destruída, apenas transformada.'
+)
+criaCartao(
+    '',
+    'Qual é a diferença entre massa e peso?',
+    'Massa é a quantidade de matéria em um objeto, medida em quilogramas (kg), enquanto peso é a força gravitacional agindo sobre essa massa'
+)
+criaCartao(
+    '',
+    'O que é aceleração centrípeta? ',
+    'Aceleração centrípeta é a aceleração que faz um objeto seguir um caminho circular, sempre direcionada para o centro do círculo.'
+)
+criaCartao(
+    '',
+    'O que é a refração da luz?',
+    'A refração da luz ocorre quando a luz muda de direção ao passar de um meio para outro com densidade diferente, alterando sua velocidade'
+)
+criaCartao(
+    '',
+    ' O que é a Lei da Gravitação Universal?',
+    ' A força de atração gravitacional entre dois objetos é proporcional ao produto de suas massas e inversamente proporcional ao quadrado da distância entre eles'
+)
+criaCartao(
+    '',
+    'O que é trabalho em física?',
+    'Trabalho é a energia transferida para um objeto quando uma força é aplicada ao longo de um deslocamento.'
+)
+criaCartao(
+    '',
+    'O que é a quantidade de movimento (momento linear)?',
+    'É o produto da massa de um objeto pela sua velocidade, dado por P=M.V, onde P é a quantidade de movimento, M  a massa e V a velocidade.'
+)
+criaCartao(
+    '',
+    'Qual é a fórmula para a energia potencial gravitacional?',
+    'Ep​=M.G.H, onde M é a massa, G é a aceleração gravitacional e H é a altura em relação a um referencial. '
+)
+criaCartao(
+    '',
+    ' Qual a Fórmula do Trabalho de uma Força?',
+    ' T = F . D, onde T é trabalho, F é força e D é deslocamento.'
+)
+
